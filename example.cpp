b7e2b173668e840d00c79fb2aa0b8bcfcdfdcf5e
GWKarnopp <gwkarnopp@yahoo.com>
2017-01-19 20:01:34 -0600

    Add files via upload

---
+++ example.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // Variable declarations
+    string name, color;
+
+    // Output and input
+    cout << "Hello, what is your name? ";
+    cin >> name;
+
+    cout << "What is your favorite color? ";
+    cin >> color;
+
+    cout << endl;
+    cout << "So can I call you " << color << " " << name << "?" << endl;
+
+	// test 1 2 3
+
+    return 0;
+}
